f5332d908064752260fde40dff237dbb311323c0
Marcos Machado Duarte <91387237+MarcosMDuarte@users.noreply.github.com>
2024-07-15 22:51:08 -0300

    Adicionar arquivos de projeto.

---
+++ Modelos/Pet.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmorDePet.Modelos
+{
+    internal class Pet
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Especie { get; set; }
+
+        public string Raca { get; set; }
+
+        public int Idade { get; set; }
+              
+        public bool Castrado { get; set; }
+
+        public bool Vacinacao { get; set; }
+
+        public bool Microchipado { get; set; }
+
+        public int Porte { get; set; }
+
+        public string Cor { get; set; }
+        public string Descricao { get; set; }
+
+        public List<string> Fotos { get; set; }
+
+
+    }
+}
